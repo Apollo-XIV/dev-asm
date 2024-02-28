92825983878adbeeb2a880df9a83c3b85222fe84
Alex Crease <alex.crease@jdplc.com>
2024-02-28 22:41:36 +0000

    typo again :)))))))

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,5 +2,5 @@
 provider "aws" {
   region                   = "eu-west-1"
   profile                  = "default"
-  shared_credentials_files = ["HOME/.aws/credentials"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
 }
