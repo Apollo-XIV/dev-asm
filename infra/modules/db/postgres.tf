d6111b2e9276c6aefd399794cc0db33439752754
Alex Crease <alexrcrease@gmail.com>
2024-03-04 11:17:41 +0000

    Update postgres.tf

--- infra/modules/db/postgres.tf
+++ infra/modules/db/postgres.tf
@@ -2,7 +2,7 @@
 resource "aws_db_instance" "default" {
   allocated_storage           = 5
   db_name                     = "forum"
-  engine                      = "postgresql"
+  engine                      = "postgres"
   instance_class              = "db.t3.micro"
   username                    = "backend"
   manage_master_user_password = true
