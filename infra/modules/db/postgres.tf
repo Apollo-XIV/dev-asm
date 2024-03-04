b793cd27871d906b46b199bb2ac1a6d53395033f
Alex Crease <alexrcrease@gmail.com>
2024-03-04 11:33:24 +0000

    nkxasdfgl

--- infra/modules/db/postgres.tf
+++ infra/modules/db/postgres.tf
@@ -6,6 +6,7 @@ resource "aws_db_instance" "default" {
   instance_class              = "db.t3.micro"
   username                    = "backend"
   manage_master_user_password = true
+  skip_final_snapshot         = true
 }
 
 output "db_cx_string" {
